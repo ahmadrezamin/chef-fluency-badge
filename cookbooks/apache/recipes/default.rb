984268abeae6efd13193f60e69e3fdd5b8ccdd3f
ramin <ahmadrezamin@yahoo.com>
2017-10-31 05:24:10 +0000

    fixed the default.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,9 +4,9 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-if node['platform_family']== "rhel"
+if node['platform_family']== "rhel" 
 	package= "httpd"
-else if node ['platform_family'] == "debian"
+elsif node ['platform_family'] == "debian"
 	package ="apache2"
 end
 
@@ -16,7 +16,6 @@ package 'apache2' do
 end
 
 service 'apache2' do
-	service_name 'httpd'
+	service_name package
 	action [:start, :enable]
 end
-
